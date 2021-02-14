f0ea4516a534e25ecb037aba88d66bea754e1c09
mario da costa bacellar <mario.bacellar@gmail.com>
2021-02-14 14:34:36 -0300

    controller

--- src/main/java/br/com/mariobacellar/geradordocid/controller/GeradorDocIdController.java
+++ src/main/java/br/com/mariobacellar/geradordocid/controller/GeradorDocIdController.java
@@ -2,7 +2,7 @@ package br.com.mariobacellar.geradordocid.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.mariobacellar.geradordocid.util.CPFHandler;
+import br.com.mariobacellar.geradordocid.util.CPFCNPJHandler;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,27 +14,39 @@ public class GeradorDocIdController {
 	@RequestMapping("/cpf")
 	@ResponseBody
 	public String getCPF() {
-		String cpf = CPFHandler.generateCPF()[0]; 
-		String json = "{\"autor\":\"https://www.linkedin.com/in/mariobacellar/\",\"aviso\":\"Gerador de CPFs inválidos. Este CPF não tem o DV válido. CPF usado somente para teste de massa de dados\",\"cpf\":\""+cpf+"\"}";
-		return json;
+		String[]  cpf     = CPFCNPJHandler.generateCPF();
+		String    cpf_num =  cpf[0];
+		String    cpf_fmt = cpf[1]; 
+		return    getJSONCPF(cpf_num,cpf_fmt);
 	}
-
-
-	@RequestMapping("/cpf/formatado")
-	@ResponseBody
-	public String getCPF_Fmt() {
-		String cpf = CPFHandler.generateCPF()[1]; 
-		String json = "{\"autor\":\"https://www.linkedin.com/in/mariobacellar/\",\"aviso\":\"Gerador de CPFs inválidos. Este CPF não tem o DV válido. CPF usado somente para teste de massa de dados\",\"cpf\":\""+cpf+"\"}";
-		return json;
-	}
-
 
 	@RequestMapping("/cnpj")
 	@ResponseBody
 	public String getCNPJ() {
-		return "Returning CNPJ";
+		String[] cnpj     = CPFCNPJHandler.generateCNPJ();
+		String   cnpj_num = cnpj[0];
+		String   cnpj_fmt = cnpj[1]; 
+		return   getJSONCNPJ(cnpj_num, cnpj_fmt);
 	}
 	
-
-
+	
+	private String getJSONCPF(String cpf, String cpf_fmt) {
+		return 	"{"
+				+ "\"autor\":\"https://www.linkedin.com/in/mariobacellar/\","
+				+ "\"gtihub\":\"https://github.com/mariobacellar/generatedocid\","						
+				+ "\"aviso\":\"Gerador de CPFs inválidos. Este CPF não tem o DV válido. CPF usado somente para teste de massa de dados\","
+				+ "\"cpf_fmt\":\""+cpf_fmt+"\","
+				+ "\"cpf_num\":\""+cpf+"\""
+				+ "}";
+	}
+	
+	private String getJSONCNPJ(String cnpj, String cnpj_fmt) {
+		return 	"{"
+				+ "\"autor\":\"https://www.linkedin.com/in/mariobacellar/\","
+				+ "\"gtihub\":\"https://github.com/mariobacellar/generatedocid\","
+				+ "\"aviso\":\"Gerador de CNPJs inválidos. Este CNPJ não tem o DV válido. CNPJ usado somente para teste de massa de dados\","
+				+ "\"cnpj_fmt\":\""+cnpj_fmt+"\","
+				+ "\"cnpj_num\":\""+cnpj+"\""
+				+ "}";
+	}	
 }
