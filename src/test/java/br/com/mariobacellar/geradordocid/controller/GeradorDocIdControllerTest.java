1512699036a95b83dbd309c84fa4adaf27d0114f
mario da costa bacellar <mario.bacellar@gmail.com>
2021-02-14 14:39:52 -0300

    sync

---
+++ src/test/java/br/com/mariobacellar/geradordocid/controller/GeradorDocIdControllerTest.java
@@ -0,0 +1,14 @@
+package br.com.mariobacellar.geradordocid.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GeradorDocIdControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/br/com/mariobacellar/geradordocid/util/CPFCNPJHandlerTest.java
@@ -0,0 +1,14 @@
+package br.com.mariobacellar.geradordocid.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CPFCNPJHandlerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
